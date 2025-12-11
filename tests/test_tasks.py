cdda6db3654e0eb82e6326df2bbbc40cae569b72
“GongWeiPc” <jiawie3@163.com>
2025-12-11 22:40:35 +0800

    添加了自动测试脚本

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -39,3 +39,69 @@ def test_list_tasks():
     assert any(t['id'] == task_id for t in tasks)
     print("返回",response_create.json())
 
+
+
+def test_filter_done():
+    payload1 = {
+        "title" : "done",
+        "is_done": True,
+    }
+    payload2 = {
+        "title":"undone",
+        "is_done":False,
+    }
+    
+    response_done = client.post("/tasks",json=payload1)
+    response_undone = client.post("/tasks",json=payload2)
+    assert response_done.status_code == 200
+    assert response_undone.status_code == 200
+
+    resp_done = client.get("/tasks",params={"done":True})
+    assert resp_done.status_code == 200
+    task_done = resp_done.json()
+    assert isinstance(task_done,list)
+    assert any(t['title'] == "done" for t in task_done)
+    assert all(t['is_done'] is True for t in task_done)
+    
+    resp_undone = client.get("/tasks",params={"done":False})
+    assert resp_undone.status_code == 200
+    task_undone = resp_undone.json()
+    assert isinstance(task_undone,list)
+    assert any(t['title'] == "undone" for t in task_undone)
+    assert all(t['is_done'] is False for t in task_undone)
+
+def test_keyword_search():
+    payload_keyword1 = {
+        "title":"vip",
+        "description" : "this is vip member",
+        "is_done": False,
+        "priority":10,
+    }
+    payload_keyword2 ={
+        "title":"normal",
+        "description":"just a normal user",
+        "is_done":False,
+        "priority":2,
+    }
+    resp_vip = client.post("/tasks",json=payload_keyword1)
+    resp_normal = client.post("/tasks",json=payload_keyword2)
+
+    assert resp_vip.status_code == 200
+    assert resp_normal.status_code == 200
+    response_search = client.get("/tasks",params={"keyword":"vip"})
+    assert response_search.status_code == 200
+    task_vip = response_search.json()
+    assert isinstance(task_vip,list)
+    assert any("vip" in t["title"] for t in task_vip)
+
+def test_skip_limit():
+    for i in range(10):
+        resp = client.post("/tasks",json={
+            "title":f"task-{i}",
+            "priority":i ,
+        })
+        assert resp.status_code == 200
+    response_skip_limit = client.get("/tasks",params={"skip":5,"limit":3})
+    assert response_skip_limit.status_code == 200
+    
+
